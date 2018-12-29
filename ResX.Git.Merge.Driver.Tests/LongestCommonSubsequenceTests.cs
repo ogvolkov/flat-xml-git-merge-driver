03170b09d578fe5916eb21767f818554e6c7cfc2
Oleg Volkov <OGVolkov@gmail.com>
2018-12-29 22:09:06 +0100

    Longest common subsequence

---
+++ ResX.Git.Merge.Driver.Tests/LongestCommonSubsequenceTests.cs
@@ -0,0 +1,106 @@
+﻿
+using System.ComponentModel;
+using Xunit;
+
+namespace ResX.Git.Merge.Driver.Tests
+{
+    public class LongestCommonSubsequenceTests
+    {
+        [Fact]
+        public void OneElementSame()
+        {
+            var lcm = Lcm("a", "a");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0 },
+                { 0, 1 }
+            });
+        }
+
+        [Fact]
+        public void OneElementDifferent()
+        {
+            var lcm = Lcm("a", "b");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0 },
+                { 0, 0 }
+            });
+        }
+
+        [Fact]
+        public void SecondHasAddedElements()
+        {
+            var lcm = Lcm("a", "ab");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0, 0 },
+                { 0, 1, 1 }
+            });
+        }
+
+        [Fact]
+        public void SecondHasRemovedElements()
+        {
+            var lcm = Lcm("ab", "a");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0 },
+                { 0, 1 },
+                { 0, 1 }
+            });
+        }
+
+        [Fact]
+        public void TwoSameElements()
+        {
+            var lcm = Lcm("ab", "ab");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0, 0 },
+                { 0, 1, 1 },
+                { 0, 1, 2 }
+            });
+        }
+
+        [Fact]
+        public void ComplexAddsAndRemoves()
+        {
+            var lcm = Lcm("xAyBC", "ABzCu");
+
+            AssertMatrix(lcm, new[,]
+            {
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 1, 1, 1 },
+                { 0, 1, 1, 1, 1, 1 },
+                { 0, 1, 2, 2, 2, 2 },
+                { 0, 1, 2, 2, 3, 3 }
+            });
+        }
+
+        private LongestCommonSubsequence<char> Lcm(string first, string second)
+        {
+            return new LongestCommonSubsequence<char>(first.ToCharArray(), second.ToCharArray());
+        }
+
+        private void AssertMatrix<T>(LongestCommonSubsequence<T> lcm, int[,] expected)
+        {
+            for (int i = 0; i < expected.GetLength(0); i++)
+            {
+                for (int j = 0; j < expected.GetLength(1); j++)
+                {
+                    Assert.True(
+                        expected[i, j] == lcm[i, j],
+                        $"[{i}, {j}] expected to be {expected[i, j]} but was {lcm[i, j]}"
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ ResX.Git.Merge.Driver/LongestCommonSubsequence.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace ResX.Git.Merge.Driver
+{
+    public class LongestCommonSubsequence<T>
+    {
+        private readonly int[,] _partialLengths;
+
+        public LongestCommonSubsequence(T[] first, T[] second)
+        {
+            _partialLengths = new int[first.Length + 1, second.Length + 1];
+
+            for (int i = 1; i <= first.Length; i++)
+            {
+                for (int j = 1; j <= second.Length; j++)
+                {
+                    if (first[i - 1].Equals(second[j - 1]))
+                    {
+                        _partialLengths[i, j] = _partialLengths[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        _partialLengths[i, j] = Math.Max(_partialLengths[i, j-1], _partialLengths[i - 1, j]);
+                    }
+                }
+            }
+        }
+
+        public int this[int i, int j] => _partialLengths[i, j];
+    }
+}
