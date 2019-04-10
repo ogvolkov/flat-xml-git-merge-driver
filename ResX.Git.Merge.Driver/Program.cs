903618746dea072be4d3d0b30a33e7df40b5e33e
Oleg Volkov <OGVolkov@gmail.com>
2019-04-10 23:46:56 +0200

    Actual command line handling to be called by git

--- ResX.Git.Merge.Driver/Program.cs
+++ ResX.Git.Merge.Driver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ResX.Git.Merge.Driver
 {
@@ -6,7 +7,36 @@ namespace ResX.Git.Merge.Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: <merge driver> <original> <current> <other>");
+                Environment.Exit(1);
+                return;
+            }
+
+            string original = args[0];
+            string current = args[1];
+            string other = args[2];
+
+            string originalContents = File.ReadAllText(original);
+            string currentContents = File.ReadAllText(current);
+            string otherContents = File.ReadAllText(other);
+
+            var merge = new ResXMerge();
+            var mergeResult = merge.Merge(originalContents, currentContents, otherContents);
+
+            switch (mergeResult)
+            {
+                case MergeConflicts _:
+                    Environment.Exit(2);
+                    return;
+                case SuccessfullyMerged successfulMerge:
+                    File.WriteAllText(current, successfulMerge.MergeResult);
+                    Environment.Exit(0);
+                    return;
+                default:
+                    throw new InvalidOperationException($"Unexpected merge result type {mergeResult.GetType()}");
+            }
         }
     }
 }
